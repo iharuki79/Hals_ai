014fe7214cacc27cb863447b0045655eba06c3d9
HalsSC <haruking.8686@gmail.com>
2019-09-29 22:47:50 +0900

    不具合の修正
    index.pyを修正しました

---
+++ index.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+import os
+from bottle import route, run
+
+@route("/")
+def hello_world():
+        return "hello world"
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
