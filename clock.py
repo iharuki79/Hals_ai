21de79be33dee0c3ae84bed9043b01df88520bf0
HalsSC <haruking.8686@gmail.com>
2019-10-02 22:20:45 +0900

    定期ツイートの周期の変更
    ツイート取得の周期を変更しました

--- clock.py
+++ clock.py
@@ -1,16 +1,28 @@
+# -*- coding: utf-8 -*-
 from apscheduler.schedulers.blocking import BlockingScheduler
-import os
 import TextTweet
 import GetTweet
 
+# APIの秘密鍵
 CK,CKS,AT,ATS=os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"], os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"]
 
 twische = BlockingScheduler()
 
+# 30分に一度ツイート
 @twische.scheduled_job('interval',minutes=30)
 def timed_job():
-    GetTweet.gettweet(CK,CKS,AT,ATS)
     TextTweet.puttweet()
+
+# 半日に一度ツイートを取得、チェーンを作ってdbに保存
+@twische.scheduled_job('interval',hours=12)
+def timed_job():
+    GetTweet.gettweet(Ck,CKS,AT,ATS)
+    f = open("data.txt",encoding="utf-8")
+    text = f.read()
+    f.close()
+    chain = PrepareChain(text)
+    triplet_freqs = chain.make_triplet_freqs()
+    chain.save(triplet_freqs, True)
 
 if __name__ == "__main__":
     twische.start()
